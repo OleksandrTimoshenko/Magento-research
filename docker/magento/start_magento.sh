517e7be8bad60f20bdb9406288c9523019432da4
Oleksandr <o.tymoshenko@astoundcommerce.com>
2023-10-28 16:46:50 +0300

    update magento domain

--- docker/magento/start_magento.sh
+++ docker/magento/start_magento.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-bin/magento setup:install --base-url=http://127.0.0.1 --db-host=db --db-name=magentodb --db-user=magentouser --db-password=1234 --admin-firstname=admin --admin-lastname=admin --admin-email=admin@admin.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1 --elasticsearch-host=elasticsearch
+/opt/wait-for-it.sh db:3306 -t 50 -- echo "Connected to DB..."
+bin/magento setup:install --base-url=http://localhost --db-host=db --db-name=$MYSQL_DATABASE --db-user=$MYSQL_USER --db-password=$MYSQL_PASSWORD --admin-firstname=$ADMIN_FIRSTNAME --admin-lastname=$ADMIN_LASTNAME --admin-email=$ADMIN_EMAIL --admin-user=$ADMIN_USER --admin-password=$ADMIN_PASS --language=$MAGENTO_LANGUAGE --currency=$MAGENTO_CURRENCY --timezone=$MAGENTO_TIMEZONE --use-rewrites=1 --elasticsearch-host=elasticsearch
 bin/magento module:disable Magento_TwoFactorAuth
 bin/magento cache:flush
-chown -R www-data. /opt/magento2
-
+chown -R www-data. /opt/magento2/var
+chown -R www-data. /opt/magento2/pub
+chown -R www-data. /opt/magento2/generated
 apt install nginx -y
 service nginx stop && service nginx start && service nginx status
 mkdir /run/php/ && touch /run/php/php8.1-fpm.sock
